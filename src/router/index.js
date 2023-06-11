a138e5fda271707d4607520f422f01b09d9e20b8
Windyna <1013155578@qq.com>
2023-06-11 20:24:01 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import('../views/login/indexView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
